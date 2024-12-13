a2adaafa2e9fa6d281a64c890eb99091a36423f5
Gor1994 <karapetyan.gor94@gmail.com>
2024-12-13 13:19:40 +0400

    update main.ts

--- nft-game-backend/src/main.ts
+++ nft-game-backend/src/main.ts
@@ -3,12 +3,23 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'https://yo-crush-repo.vercel.app', // Allow your frontend domain
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
-    credentials: true, // If you need cookies/authentication
-  });
-  await app.listen(3001);
+
+  // Enable CORS
+  const corsOptions = {
+    origin:
+      process.env.NODE_ENV === 'production'
+        ? 'https://yo-crush-repo.vercel.app' // Allow only frontend domain in production
+        : true, // Allow all origins during development
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Allow cookies/authentication
+  };
+  app.enableCors(corsOptions);
+
+  // Start the server
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
+
+  console.log(`Backend running at port ${port}`);
 }
 
 bootstrap();
