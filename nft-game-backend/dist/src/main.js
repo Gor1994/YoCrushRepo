4263c4f57f9e40e7bbea7d8b160f7a481dcfedb9
Gor1994 <karapetyan.gor94@gmail.com>
2024-12-13 13:09:39 +0400

    fix

--- nft-game-backend/dist/src/main.js
+++ nft-game-backend/dist/src/main.js
@@ -2,16 +2,25 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    app.enableCors({
-        origin: true,
+
+    // Enable CORS
+    const corsOptions = {
+        origin: process.env.NODE_ENV === 'production'
+            ? 'https://yo-crush-repo.vercel.app' // Allow only your frontend domain in production
+            : true, // Allow all origins during development
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         credentials: true,
-    });
-    const port = process.env.PORT || 3001;
+    };
+    app.enableCors(corsOptions);
+
+    // Start the server
+    const port = process.env.PORT || 3000;
     await app.listen(port);
-    console.log(`Backend running at http://localhost:${port}`);
+
+    console.log(`Backend running at port ${port}`);
 }
+
 bootstrap();
-//# sourceMappingURL=main.js.map
