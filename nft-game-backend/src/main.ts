f4664939995813aef2d5a99b332b1564c5c203a3
Gor1994 <karapetyan.gor94@gmail.com>
2024-12-13 15:27:53 +0400

    ...

--- nft-game-backend/src/main.ts
+++ nft-game-backend/src/main.ts
@@ -16,6 +16,11 @@ async function bootstrap() {
   await app.listen(port);
 
   console.log(`Application is running on: http://localhost:${port}`);
+
+  app.use((req, res, next) => {
+    console.log(`Incoming request: ${req.method} ${req.url}`);
+    next();
+  });
 }
 
 bootstrap();
